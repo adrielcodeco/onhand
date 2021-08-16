414d0042a5684d69388dda06a702cd09d125430a
Codeco <adrielcodeco@hotmail.com>
2021-08-16 16:51:13 -0300

    fix: packages files

--- packages/workspace/src/commands/createCommand.ts
+++ packages/workspace/src/commands/createCommand.ts
@@ -4,52 +4,65 @@ import { createWorkspace } from '#/lib/createWorkspace'
 
 export async function createCommand (options: { verbose: boolean }) {
   const { verbose } = options
-  const defaults = { repositoriesFolder: 'repositories', githubLogin: '' }
-  const answers = await prompts([
-    {
-      type: 'text',
-      name: 'workspaceName',
-      message: 'What will be the workspace name ?',
-    },
-    {
-      type: 'text',
-      name: 'repositoriesFolder',
-      message: 'What will be the name of the repositories folder ?',
-      initial: defaults.repositoriesFolder,
-    },
-    {
-      type: 'confirm',
-      name: 'pushToGit',
-      message: 'Do you want to push it to github ?',
-      initial: true,
-    },
-    {
-      type: prev => (prev === true ? 'password' : null),
-      name: 'githubAccessToken',
-      message: 'What Github Personal Access Token can I use ?',
-      validate: async value => {
-        const login = await checkAccess(value, verbose)
-        defaults.githubLogin = login
-        return !!login
+  const defaults = {
+    workspaceName: 'workspace',
+    repositoriesFolder: 'repositories',
+    githubLogin: '',
+  }
+  const answers = await prompts(
+    [
+      {
+        type: 'text',
+        name: 'workspaceName',
+        message: 'What will be the workspace name ?',
+        initial: defaults.workspaceName,
       },
-      // @ts-expect-error
-      error: 'Invalid Github Personal Access Token',
-    },
+      {
+        type: 'text',
+        name: 'repositoriesFolder',
+        message: 'What will be the name of the repositories folder ?',
+        initial: defaults.repositoriesFolder,
+      },
+      {
+        type: 'confirm',
+        name: 'pushToGit',
+        message: 'Do you want to push it to github ?',
+        initial: true,
+      },
+      {
+        type: prev => (prev === true ? 'password' : null),
+        name: 'githubAccessToken',
+        message: 'What Github Personal Access Token can I use ?',
+        validate: async value => {
+          const login = await checkAccess(value, verbose)
+          defaults.githubLogin = login
+          return !!login
+        },
+        // @ts-expect-error
+        error: 'Invalid Github Personal Access Token',
+      },
+      {
+        type: prev => (prev ? 'text' : null),
+        name: 'githubProjectName',
+        message: 'What will be the Github project name ?',
+        initial: (prev, values, prompt) => {
+          return values.workspaceName
+        },
+      },
+      {
+        type: prev => (prev ? 'confirm' : null),
+        name: 'githubPrivateRepo',
+        message: 'Do you want to make the repo private ?',
+        initial: true,
+      },
+    ],
     {
-      type: prev => (prev ? 'text' : null),
-      name: 'githubProjectName',
-      message: 'What will be the Github project name ?',
-      initial: (prev, values, prompt) => {
-        return values.workspaceName
+      onCancel: () => {
+        console.log('canceled by user')
+        process.exit(1)
       },
     },
-    {
-      type: prev => (prev ? 'confirm' : null),
-      name: 'githubPrivateRepo',
-      message: 'Do you want to make the repo private ?',
-      initial: true,
-    },
-  ])
+  )
   const fullOptions = Object.assign({}, defaults, options, answers)
   if (!fullOptions.workspaceName) {
     throw new Error('The workspace name is required')
