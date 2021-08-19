39b44a354a78d8966cf5675d1429ddf11c61fb41
Codeco <adrielcodeco@hotmail.com>
2021-08-19 14:36:14 -0300

    fix: iac-docker dependencies

--- packages/iac-docker/src/commands/serve.ts
+++ packages/iac-docker/src/commands/serve.ts
@@ -1,5 +1,3 @@
-import path from 'path'
-import execa from 'execa'
 import { loadConfig } from '#/app/loadConfig'
 import { serve } from '#/app/serve'
 
@@ -12,24 +10,5 @@ export async function serveCommand (
   },
 ) {
   const options = loadConfig({}, configPath)
-  if (serverOptions?.setupDB) {
-    console.log('setupDB=true')
-    await execa(
-      'node',
-      [
-        '--trace-warnings',
-        path.resolve(__dirname, '../../bin/iac'),
-        'seed',
-        ...(configPath ? ['--config', configPath] : []),
-      ],
-      {
-        stdio: ['inherit', 'inherit', 'inherit', 'ipc'],
-        cwd: process.cwd(),
-        env: {
-          ...process.env,
-        },
-      },
-    )
-  }
   await serve(options, serverOptions)
 }
