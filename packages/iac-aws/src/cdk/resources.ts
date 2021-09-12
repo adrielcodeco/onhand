b691e980f54bc31a065da69378da6e5ddcf3956d
Codeco <adrielcodeco@hotmail.com>
2021-09-11 22:36:36 -0300

    fix(iac-aws): stack name

--- packages/iac-aws/src/cdk/resources.ts
+++ packages/iac-aws/src/cdk/resources.ts
@@ -5,12 +5,12 @@ export const projectName = (options: Options) =>
   options.config?.app?.projectName ??
   options.config?.app?.name ??
   options.packageName ??
-  _.camelCase(`onhand-${options.config?.app?.type}`)
+  _.kebabCase(`onhand-${options.config?.app?.type}`)
 
 export const s3Arn = (bucketName: string) => `arn:aws:s3:::${bucketName}`
 
 export const getS3StackName = (options: Options) =>
-  resourceName(options, `${options.config?.app?.type} assets`)
+  resourceName(options, `${options.config?.app?.type} assets`, false, 'kebab')
 
 export const getAssetsBucketName = (options: Options) =>
   resourceName(options, `${options.config?.app?.type} assets`)
@@ -19,19 +19,19 @@ export const getReleasesBucketName = (options: Options) =>
   resourceName(options, `${options.config?.app?.type} releases`, true, 'kebab')
 
 export const getMainStackName = (options: Options) =>
-  resourceName(options, options.config?.app?.type!)
+  resourceName(options, options.config?.app?.type!, false, 'kebab')
 
 export const getFunctionsStackName = (options: Options) =>
-  resourceName(options, 'functions')
+  resourceName(options, 'functions', false, 'kebab')
 
 export const getApiGatewayStackName = (options: Options) =>
-  resourceName(options, 'apigateway')
+  resourceName(options, 'apigateway', false, 'kebab')
 
 export const getDeployStackName = (options: Options) =>
-  resourceName(options, `${options.config?.app?.type} deploy`)
+  resourceName(options, `${options.config?.app?.type} deploy`, false, 'kebab')
 
 export const getCloudFormationStackName = (options: Options) =>
-  resourceName(options, 'cloudfront')
+  resourceName(options, 'cloudfront', false, 'kebab')
 
 export const getCFDistributionName = (options: Options) =>
   resourceName(options, 'site distribution')
