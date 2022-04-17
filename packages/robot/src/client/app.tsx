280c43e96151a06046510a1b4e05c004e0adba6a
Codeco <adrielcodeco@hotmail.com>
2022-04-17 15:59:19 -0300

    fix: typecheck

--- packages/robot/src/client/app.tsx
+++ packages/robot/src/client/app.tsx
@@ -1,19 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+// import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import { createTheme, ThemeProvider } from '@material-ui/core/styles'
-import CircularProgress from '@material-ui/core/CircularProgress'
+// import CircularProgress from '@material-ui/core/CircularProgress'
 import deepPurple from '@material-ui/core/colors/deepPurple'
 import yellow from '@material-ui/core/colors/yellow'
-import { SnackbarProvider } from 'notistack'
+// import { SnackbarProvider } from 'notistack'
 import { ConfigContext } from '~/contexts/config'
 import { configService } from '~/services/config'
-import { Container } from '~/components/templates/container'
-import { DeploySetup } from '~/components/pages/deploy/deploy-setup'
-import { ProjectList } from '~/components/pages/projects/project-list'
-import { ProjectAdd } from '~/components/pages/projects/project-add'
-import { ProjectDetails } from '~/components/pages/projects/project-details'
-import { Index } from '~/components/pages/index'
+// import { Container } from '~/components/templates/container'
+// import { DeploySetup } from '~/components/pages/deploy/deploy-setup'
+// import { ProjectList } from '~/components/pages/projects/project-list'
+// import { ProjectAdd } from '~/components/pages/projects/project-add'
+// import { ProjectDetails } from '~/components/pages/projects/project-details'
+// import { Index } from '~/components/pages/index'
 
 const theme = createTheme({
   palette: {
@@ -30,7 +30,7 @@ export const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <ConfigContext.Provider value={config}>
-        <SnackbarProvider
+        {/* <SnackbarProvider
           maxSnack={3}
           anchorOrigin={{
             vertical: 'bottom',
@@ -63,7 +63,7 @@ export const App = () => {
                   )}
             </Container>
           </BrowserRouter>
-        </SnackbarProvider>
+        </SnackbarProvider> */}
       </ConfigContext.Provider>
     </ThemeProvider>
   )
